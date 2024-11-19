3eb2ce10e9112e72a7f74529f84d9234c3686088
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2024-11-19 20:28:21 -0300

    17. Docstrings como comentários

--- logica_programacao/aula1.py
+++ logica_programacao/aula1.py
@@ -1,4 +1,13 @@
+"""
+DocString
+E escrever o que eu 
+quiser
+"""
+'''
+Usar para escrever suas notas
+'''
+
 # Permite escrever um comentário
 print(123) # Na frente
 # Abaixo
-print(123)
+print(456)
