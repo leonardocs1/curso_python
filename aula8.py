d5b7b15f231425e5b807083454cc69065251409f
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2024-11-20 22:09:21 -0300

    24. Exercício: variáveis e tipos de dados para treino

---
+++ aula8.py
@@ -0,0 +1,12 @@
+nome = 'Leonardo'
+sobrenome = 'Costa Santos'
+idade = 32
+ano_nascimento = 1992
+maior_idade = idade >= 18
+altura_metros = 1.80
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('É maior de idade?', maior_idade)
+print('Altura em metros:', altura_metros)
